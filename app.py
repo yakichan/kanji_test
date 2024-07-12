ba8f3c5b92662e289078a7cc280614e95fe9616b
Sung Ji Soo <milkylicious@gmail.com>
2024-07-12 16:34:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, render_template, session, redirect, url_for
 import pandas as pd
+from flask import Flask, request, render_template, session, redirect, url_for
 import random
-import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
